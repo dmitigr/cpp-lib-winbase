c8d0b71405774abca1a57bdf916c38fa53963d5e
Dmitry Igrishin <dmitigr@gmail.com>
2024-03-07 18:10:53 +0300

    Fix

--- detail.hpp
+++ detail.hpp
@@ -14,10 +14,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma once
+
 #include <array>
 #include <type_traits>
-
-#pragma once
 
 namespace dmitigr::winbase::detail {
 
